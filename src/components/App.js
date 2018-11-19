dba177ca5a6b5842d265adbbb21b5e62fbab09a1
Eddie Padin <eddie@entertainermedia.net>
2018-11-19 15:46:45 -0600

    feat:  app data initialization; - add logic to load initial data when App component is mounted; - add connect export to App component;

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { handleInitialData } from "../actions/shared";
+import { connect } from "react-redux";
 
 class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
+  }
+
   render() {
     return (
-      <div>
-        Starter Code
+      <div className="main">
+        <div className="container">Starter Code</div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default connect()(App);
