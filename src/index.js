3e1f6d42c030f5bcb98cf999b56dbae36c173673
Eddie Padin <eddie@entertainermedia.net>
2018-11-19 15:09:59 -0600

    feat:  add middleware; - add thunk middleware; - add logger middleware; - implement middleware with redux createStore;

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import middleware from "./middleware";
 import App from "./components/App";
 import "./index.css";
 
 //create store const
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <Provider store={store}>
