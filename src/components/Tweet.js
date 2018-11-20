44b1615ddda7e8277b9858a8b526695a13828552
Eddie Padin <eddie@entertainermedia.net>
2018-11-20 09:25:57 -0600

    feature: tweet component; - add parent/child relation logic for replies; - add condition to display notification if tweet doesn't exist;

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,11 @@ import { formatTweet } from "../utils/helpers";
 class Tweet extends Component {
   render() {
     const { tweet } = this.props;
+    //if tweet doesn't exist, only return notification
+    if (tweet === null) {
+      return <p>This tweet doesn't exist.</p>;
+    }
+
     return (
       <div>
         <div>Tweet Id: {tweet.id}</div>
@@ -26,11 +31,20 @@ class Tweet extends Component {
 function mapStateToProps({ authUser, users, tweets }, { id }) {
   //get the current tweet from the tweets store state by id
   const tweet = tweets[id];
+
+  //get parent/child tweet relation if exists, otherwise return null
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
+
+  //format the tweet if exists, otherwise return null
+  const formattedTweet = tweet
+    ? formatTweet(tweet, users[tweet.author], authUser, parentTweet)
+    : null;
+
   //pass from store to component
   return {
     authUser,
     //get the tweet, author of the tweet & the current auth user
-    tweet: formatTweet(tweet, users[tweet.author], authUser)
+    tweet: formattedTweet
   };
 }
 
