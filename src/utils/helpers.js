53caadba2a9d2a43d00ec524b048686412da441a
Eddie Padin <eddie@entertainermedia.net>
2018-11-20 11:44:53 -0600

    feature: Tweet date format; - add moment.js support; - fix issues with formatDate helper using moment;

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,25 +1,46 @@
-export function formatDate (timestamp) {
-  const d = new Date(timestamp)
-  const time = d.toLocaleTimeString('en-US')
-  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+import moment from "moment";
+
+/**
+ * Format date
+ * @example h:i A | mm/dd/yyyy
+ * @param timestamp
+ * @return {string}
+ */
+export function formatDate(timestamp) {
+  const d = new Date(timestamp);
+
+  const formattedDate = moment
+    .utc(d)
+    .local()
+    .format("M/DD/YYYY");
+
+  const formattedTime = moment
+    .utc(timestamp)
+    .local()
+    .format("h:mm A");
+
+  return formattedTime + " | " + formattedDate;
 }
 
-export function formatTweet (tweet, author, authUser, parentTweet) {
-  const { id, likes, replies, text, timestamp } = tweet
-  const { name, avatarURL } = author
+export function formatTweet(tweet, author, authUser, parentTweet) {
+  const { id, likes, replies, text, timestamp } = tweet;
+  const { name, avatarURL } = author;
+  const formattedTimestamp = formatDate(timestamp);
 
   return {
     name,
     id,
-    timestamp,
     text,
+    timestamp: formattedTimestamp,
     avatar: avatarURL,
     likes: likes.length,
     replies: replies.length,
     hasLiked: likes.includes(authUser),
-    parent: !parentTweet ? null : {
-      author: parentTweet.author,
-      id: parentTweet.id,
-    }
-  }
+    parent: !parentTweet
+      ? null
+      : {
+          author: parentTweet.author,
+          id: parentTweet.id
+        }
+  };
 }
