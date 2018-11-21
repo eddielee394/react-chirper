35cb9317eb5a1aeafa461c7e6c732788ebef4ed3
Eddie Padin <eddie@entertainermedia.net>
2018-11-21 11:59:49 -0600

    feat: routing; - add routing functionality; - refactor import statements; - add NavigationBar component to App container; - update condition to show Loader component while state is loading; - add initial routes to components;

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
+//Dependencies
 import React, { Component } from "react";
-import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
-import Dashboard from "./Dashboard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoadingBar from "react-redux-loading-bar";
 import { Slide, ToastContainer } from "react-toastify";
+
+//Custom components
+import Loader from "./Loader/Loader";
+import NewTweet from "./NewTweet/NewTweet";
+import NavigationBar from "./NavigationBar";
+import Dashboard from "./Dashboard";
 import TweetPage from "./TweetPage";
+
+//imported methods/functions
+import { handleInitialData } from "../actions/shared";
 
 class App extends Component {
   componentDidMount() {
@@ -14,22 +23,34 @@ class App extends Component {
 
   render() {
     const { loading } = this.props;
-    //conditionally display dashboard if state isn't loading
-    let dashboard = loading ? null : <Dashboard />;
 
     return (
-      <div className="main">
-        <LoadingBar />
-        <div className="container">
-          {dashboard}
-          <TweetPage match={{ params: { id: "2mb6re13q842wu8n106bhk" } }} />
+      <Router>
+        <div className="main h-100v">
+          <LoadingBar />
+          <NavigationBar />
+          {loading === true ? (
+            <div className="container d-flex justify-content-center h-100">
+              <div className="row justify-content-center align-self-center">
+                <div className="col-12">
+                  <Loader />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="container">
+              <Route exact path="/" component={Dashboard} />
+              <Route exact path="/new" component={NewTweet} />
+              <Route exact path="/tweet/:id" component={TweetPage} />
+            </div>
+          )}
+          <ToastContainer
+            transition={Slide}
+            newestOnTop={true}
+            hideProgressBar={true}
+          />
         </div>
-        <ToastContainer
-          transition={Slide}
-          newestOnTop={true}
-          hideProgressBar={true}
-        />
-      </div>
+      </Router>
     );
   }
 }
