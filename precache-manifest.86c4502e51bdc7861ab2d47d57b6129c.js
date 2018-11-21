db2694b7aed0c057d2f72f76e334592e59d34b9a
Eddie Padin <eddie@entertainermedia.net>
2018-11-21 17:20:59 -0600

    Updates

---
+++ precache-manifest.86c4502e51bdc7861ab2d47d57b6129c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2a68463183a5f5c5293",
+    "url": "/react-chirper/static/js/runtime~main.a2a68463.js"
+  },
+  {
+    "revision": "99592aa760e837ee39d0",
+    "url": "/react-chirper/static/js/main.99592aa7.chunk.js"
+  },
+  {
+    "revision": "d0c462bd1d1434134480",
+    "url": "/react-chirper/static/js/1.d0c462bd.chunk.js"
+  },
+  {
+    "revision": "99592aa760e837ee39d0",
+    "url": "/react-chirper/static/css/main.d20a8d81.chunk.css"
+  },
+  {
+    "revision": "d0c462bd1d1434134480",
+    "url": "/react-chirper/static/css/1.4ad2c871.chunk.css"
+  },
+  {
+    "revision": "59abbe77ccf17d8f6d8262326aec8bc8",
+    "url": "/react-chirper/index.html"
+  }
+];
