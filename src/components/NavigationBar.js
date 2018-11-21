b3f142dd083ab821e3e5ae8cb61323033b7fa676
Eddie Padin <eddie@entertainermedia.net>
2018-11-21 11:59:38 -0600

    feat: NavigationBar component; - add navigation bar;

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+const NavigationBar = props => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <LinkContainer exact to="/" activeClassName="nav-brand-active">
+        <Navbar.Brand>React-Chirper</Navbar.Brand>
+      </LinkContainer>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer exact to="/">
+            <Nav.Link>Dashboard</Nav.Link>
+          </LinkContainer>
+          <LinkContainer exact to="/new">
+            <Nav.Link>New Tweet</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavigationBar;
